62611becdb05f988a92f99e7702e69efe03ad6e1
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: put octos variables in the "Octos" struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
+# Insert new variables inside the Octos structure
 octos_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Octos": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
