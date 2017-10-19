DirtyUnicorns/android_vendor_du

Add branding.mk
Change-Id: Ic9848e13ddb4edda19d2e68ae1913b9d84e2be8b

config/common.mk
@@ -32,3 +32,6 @@ PRODUCT_COPY_FILES += \
 
 # Packages
 include vendor/du/config/packages.mk
+
+# Branding
+include vendor/du/config/branding.mk

TeamOctOS/android_vendor_octos

config/common.mk
@@ -32,3 +32,6 @@ PRODUCT_COPY_FILES += \
 
 # Packages
 include vendor/octos/config/packages.mk
+
+# Branding
+include vendor/octos/config/branding.mk