ee78504cca82debd155779c44107e42f02d8b469
jason972000 <jason972000@gmail.com>
2017-12-20 14:34:42 -0600

    vendor: add serranolteusc to vendorsetup vendor: add serranoltexx  to vendorsetup vendor: add serrano3gxx   to vendorsetup vendor: add serranodsdd   to vendorsetup
    Change-Id: I13b496b787ec8f6d3ad4e6097a0fc8fd390d6149

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,9 @@
 add_lunch_combo octos_angler-userdebug
 add_lunch_combo octos_bullhead-userdebug
 add_lunch_combo octos_oneplus3-userdebug
+add_lunch_combo octos_serrano3gxx-userdebug
+add_lunch_combo octos_serranodsdd-userdebug
+add_lunch_combo octos_serranolteusc-userdebug
+add_lunch_combo octos_serranoltexx-userdebug
 add_lunch_combo octos_shamu-userdebug
 add_lunch_combo octos_taimen-userdebug
