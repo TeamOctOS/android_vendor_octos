c7d9acc09300ad29c9bba4285a538cec735717ad
ujwalp15 <ujwalp09@gmail.com>
2017-12-24 11:31:16 +0530

    vendor: add oneplus3 to vendorsetup
    Change-Id: I8edaf6ef2dea7cfff1750f92bd4cd2116c3cbe47

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo octos_angler-userdebug
 add_lunch_combo octos_bullhead-userdebug
+add_lunch_combo octos_oneplus3-userdebug
 add_lunch_combo octos_shamu-userdebug
 add_lunch_combo octos_taimen-userdebug
